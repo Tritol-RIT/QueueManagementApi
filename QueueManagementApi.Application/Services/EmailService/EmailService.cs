8876ba653d80a9c3b5744de45277711fe8c0486c
Veton Klinaku <vk8248@g.rit>
2024-02-28 22:22:57 +0100

    Basic email send

---
+++ QueueManagementApi.Application/Services/EmailService/EmailService.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Mail;
+using System.Net;
+namespace QueueManagementApi.Application.Services.EmailService;
+
+
+public class EmailService : IEmailService
+{
+    private const string _smtpServer = "smtp.gmail.com";
+    private const int _smtpPort = 587;
+    private const string _smtpUsername = "denivetoni@gmail.com";
+    private const string _smtpPassword = "rgapduuvyqjzghkk";
+    private const string attachmentFilePath = "C:\\Users\\vetonk\\Downloads\\test.txt";
+    public async Task SendEmailAsync(string email, string subject, string body)
+    {
+        using (var client = new SmtpClient())
+        {
+            client.Host = _smtpServer;
+            client.Port = _smtpPort;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential(_smtpUsername, _smtpPassword);
+            using (var message = new MailMessage(
+                from: new MailAddress(_smtpUsername, "RITK Queue Managment"),
+                to: new MailAddress(email, "Visitor")
+                ))
+            {
+
+                message.Subject = subject;
+                message.Body = body;
+
+                var attachment = new Attachment(attachmentFilePath);
+                message.Attachments.Add(attachment);
+
+                await client.SendMailAsync(message);
+            }
+        }
+    }
+}
+
---
+++ QueueManagementApi/Controllers/EmailController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QueueManagementApi.Application.Services;
+using QueueManagementApi.Application.Services.EmailService;
+using QueueManagementApi.Core.Entities;
+
+namespace QueueManagementApi.Controllers;
+
+[Route("email")]
+public class EmailController : ApiController
+{
+    private readonly IEmailService _emailService;
+    public EmailController(IEmailService emailService)
+    {
+        _emailService = emailService;
+    }
+    // POST /email/send
+    [HttpPost("send")]
+    public async Task<ActionResult> SendEmail(string email, string subject, string message)
+    {
+        await _emailService.SendEmailAsync(email, subject, message);
+        return Ok();
+    }
+}
