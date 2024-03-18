533aa9171db64e4ea914d0c034b2961e12adfad3
detib <detibaholli@gmail.com>
2024-03-18 17:42:09 +0100

    change constructors of paged list

--- QueueManagementApi.Entity/PagedList.cs
+++ QueueManagementApi.Entity/PagedList.cs
@@ -4,35 +4,31 @@
 public class PagedList<T> : List<T>
 {
     public int CurrentPage { get; }
-    public int TotalPages { get; }
+    public int TotalPages { get;  }
     public int PageSize { get; private set; }
-    public int TotalCount { get; private set; }
+    public int TotalCount { get; }
 
     public bool HasPrevious => CurrentPage > 1;
     public bool HasNext => CurrentPage < TotalPages;
 
-    public PagedList(List<T> items, int count, int pageNumber, int pageSize)
+    public PagedList(IQueryable<T> source, int pageNumber, int pageSize)
     {
-        TotalCount = count;
+        TotalCount = source.Count();
         PageSize = pageSize > 0 ? pageSize : 10;
         CurrentPage = pageNumber > 1 ? pageNumber : 1;
-        TotalPages = (int)Math.Ceiling(count / (double)pageSize);
-
+        TotalPages = (int)Math.Ceiling(TotalCount / (double)pageSize);
+        var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
         AddRange(items);
     }
 
-    public static PagedList<T> Create(IQueryable<T> source, int pageNumber, int pageSize)
+    public PagedList(IEnumerable<T> source, int pageNumber, int pageSize)
     {
-        var count = source.Count();
-        var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
-        return new PagedList<T>(items, count, pageNumber, pageSize);
-    }
-
-    public static PagedList<T> Create(IEnumerable<T> source, int pageNumber, int pageSize)
-    {
-        var enumerable = source as T[] ?? source.ToArray();
-        var count = enumerable.Length;
-        var items = enumerable.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
-        return new PagedList<T>(items, count, pageNumber, pageSize);
+        var items = source as IList<T> ?? source.ToList();
+        TotalCount = items.Count;
+        PageSize = pageSize > 0 ? pageSize : 10;
+        CurrentPage = pageNumber > 1 ? pageNumber : 1;
+        TotalPages = (int)Math.Ceiling(TotalCount / (double)pageSize);
+        var enumerableItems = items.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+        AddRange(enumerableItems);
     }
 }
