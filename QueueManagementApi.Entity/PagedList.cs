e74ace6cc57ab41151e071291a03c1c82c3318a6
detib <detibaholli@gmail.com>
2024-03-18 17:24:57 +0100

    add Paged list class for paginated results

---
+++ QueueManagementApi.Entity/PagedList.cs
@@ -0,0 +1,38 @@
+﻿namespace QueueManagementApi.Core;
+
+
+public class PagedList<T> : List<T>
+{
+    public int CurrentPage { get; }
+    public int TotalPages { get; }
+    public int PageSize { get; private set; }
+    public int TotalCount { get; private set; }
+
+    public bool HasPrevious => CurrentPage > 1;
+    public bool HasNext => CurrentPage < TotalPages;
+
+    public PagedList(List<T> items, int count, int pageNumber, int pageSize)
+    {
+        TotalCount = count;
+        PageSize = pageSize > 0 ? pageSize : 10;
+        CurrentPage = pageNumber > 1 ? pageNumber : 1;
+        TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+        AddRange(items);
+    }
+
+    public static PagedList<T> Create(IQueryable<T> source, int pageNumber, int pageSize)
+    {
+        var count = source.Count();
+        var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+        return new PagedList<T>(items, count, pageNumber, pageSize);
+    }
+
+    public static PagedList<T> Create(IEnumerable<T> source, int pageNumber, int pageSize)
+    {
+        var enumerable = source as T[] ?? source.ToArray();
+        var count = enumerable.Length;
+        var items = enumerable.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+        return new PagedList<T>(items, count, pageNumber, pageSize);
+    }
+}
