3c8495aaa3782d1d8f8f95fb9f6b3012e358fb59
Veton Klinaku <vk8248@g.rit>
2024-05-06 20:15:02 +0200

    Basic structure

---
+++ QueueManagementApi/Controllers/CheckinController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QueueManagementApi.Core.Entities;
+using QueueManagementApi.Application.Services.CheckinService;
+using QueueManagementApi.Application.Services.QrCodeService;
+
+namespace QueueManagementApi.Controllers;
+
+[Route("checkin")]
+public class CheckinController : ApiController
+{
+
+    public CheckinController()
+    {
+    }
+    [HttpGet("StartCheckin")]
+    public ActionResult<List<Exhibit>> StartCheckin(int exhibitId,string QrCodeGUID)
+    {
+        return Ok();
+    }
+
+}
